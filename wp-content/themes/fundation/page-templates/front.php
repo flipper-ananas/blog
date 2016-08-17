e91cf8d4ddc06c5ce9b285623f4dfc19ea3f24d5
flipper-ananas <pierre.brenot@gmail.com>
2016-08-17 15:03:55 +0200

    initialisation

---
+++ wp-content/themes/fundation/page-templates/front.php
@@ -0,0 +1,88 @@
+<?php
+/*
+Template Name: Front
+*/
+get_header(); ?>
+
+<header id="front-hero" role="banner">
+	<div class="marketing">
+		<div class="tagline">
+			<h1><?php bloginfo( 'name' ); ?></h1>
+			<h4 class="subheader"><?php bloginfo( 'description' ); ?></h4>
+		</div>
+
+		<div id="watch">
+			<section id="twitter">
+				<a href="https://twitter.com/edd_br">@Edd_Br</a>
+			</section>
+		</div>
+	</div>
+
+</header>
+
+<?php do_action( 'foundationpress_before_content' ); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+<section class="intro" role="main">
+	<div class="fp-intro">
+
+		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+			<div class="entry-content">
+				<?php the_excerpt(); ?>
+			</div>
+			<footer>
+				<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+				<p><?php the_tags(); ?></p>
+			</footer>
+			<?php do_action( 'foundationpress_page_before_comments' ); ?>
+			<?php comments_template(); ?>
+			<?php do_action( 'foundationpress_page_after_comments' ); ?>
+		</div>
+
+	</div>
+
+</section>
+<?php endwhile;?>
+<?php do_action( 'foundationpress_after_content' ); ?>
+
+<div class="section-divider">
+	<hr />
+</div>
+
+<div id="single-post" role="main">
+
+<?php do_action( 'foundationpress_before_content' ); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+	<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
+		<header>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<?php foundationpress_entry_meta(); ?>
+		</header>
+		<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
+		<div class="entry-content">
+
+		<?php if ( has_post_thumbnail() ) : ?>
+			<div class="row">
+				<div class="column">
+					<?php the_post_thumbnail( '', array('class' => 'th') ); ?>
+				</div>
+			</div>
+		<?php endif; ?>
+
+		<?php the_excerpt(); ?>
+		</div>
+		<footer>
+			<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+			<p><?php the_tags(); ?></p>
+		</footer>
+		<?php do_action( 'foundationpress_post_before_comments' ); ?>
+		<?php comments_template(); ?>
+		<?php do_action( 'foundationpress_post_after_comments' ); ?>
+	</article>
+<?php endwhile;?>
+
+<?php do_action( 'foundationpress_after_content' ); ?>
+<?php get_sidebar(); ?>
+</div>
+
+<?php get_footer();
